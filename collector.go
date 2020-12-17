430eaea67f4edbfe3909f19a3f419c90e2d3b93b
ibere.tizio <ibere.tizio@tivit.com>
2020-12-17 16:38:14 -0300

    altecao

--- collector.go
+++ collector.go
@@ -91,12 +91,13 @@ func ScrapeTarget(ctx context.Context, target string, config *config.Module, ibc
 	snmp.Retries = config.WalkParams.Retries
 	snmp.Timeout = config.WalkParams.Timeout
 	if len(ibcommunity) > 0 {
-		snmp.Community = ibcommunity
+	//	snmp.Community = ibcommunity
+		snmp.SetCommunity(ibcommunity)
 	}
 
 	snmp.Target = target
 	level.Info(logger).Log("msg", "Parametros -IBCheck(Collect): ", "target", target, "Community", snmp.Community)
-	level.Info(logger).Log("Debug", snmp)
+	level.Info(logger).Log("Debug", fmt.Printf("%v", snmp))
 	snmp.Port = 161
 	if host, port, err := net.SplitHostPort(target); err == nil {
 		snmp.Target = host
