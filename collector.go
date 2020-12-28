a12de7da12ced0577dcc5014ed4f8a2364934bed
ibere.tizio <ibere.tizio@tivit.com>
2020-12-28 15:35:31 -0300

    altecao

--- collector.go
+++ collector.go
@@ -121,7 +121,17 @@ func ScrapeTarget(ctx context.Context, target string, config *config.Module, ibc
 	defer snmp.Conn.Close()
 
 	result := []gosnmp.SnmpPDU{}
-	getOids := config.Get
+	getOids := []string{}
+	for _, elget := range config.Get {
+		fimget := elget
+		if len(ibports) > 0 {
+			for _, elport := range strings.Split(ibports, ",") {
+				fimget = fimget + "." + elport
+			}
+		}
+		getOids = append(getOids, fimget)
+	}
+	//getOids := config.Get
 	level.Info(logger).Log("msg", "Parametros -IBDebug: ", "Ports", ibports, "OIDs", strings.Join(getOids, ","))
 	maxOids := int(config.WalkParams.MaxRepetitions)
 	// Max Repetition can be 0, maxOids cannot. SNMPv1 can only report one OID error per call.
